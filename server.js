cf9a42626ed8a0537a533b730fe852ec215b27e4
vicky <vicky@d3j.io>
2017-04-24 18:57:58 +0800

    init

---
+++ server.js
@@ -0,0 +1,139 @@
+var net = require("net");
+var io = require("io");
+var mq = require("mq");
+
+var config = {
+    timeout: 5 * 1000,
+    domain: "d3j.io",
+    port: 9980,
+    error_domain: `unknown domain`,
+    error_timeout: `server not online`
+};
+
+function ConnPool(is_client) {
+    var pool = {};
+
+    this.get = (host) => {
+        var conns = pool[host];
+        if (!conns)
+            return;
+
+        var conn = conns.pop();
+        if (conns.length == 0)
+            delete pool[host];
+
+        return conn;
+    }
+
+    this.put = (host, c) => {
+        var conns = pool[host];
+        if (!conns)
+            pool[host] = conns = [];
+
+        c.date = new Date();
+        conns.push(c);
+    }
+
+    // timeout
+    setInterval(() => {
+        var now = new Date();
+
+        for (var k in pool) {
+            var conns = pool[k];
+            while (conns.length > 0) {
+                var conn = conns[0];
+                if (now - conn.date > config.timeout) {
+                    if (is_client) {
+                        conn.write(config.error_timeout,
+                            () => conn.aw.end());
+                    } else
+                        conn.aw.end();
+                    conns = conns.slice(1);
+                } else
+                    break;
+            }
+
+            if (conns.length == 0)
+                delete pool[k];
+        }
+    }, 200);
+}
+
+var servers = new ConnPool();
+var clients = new ConnPool(true);
+
+function link(server, client) {
+    function end() {
+        server.aw.end();
+        client.aw.end();
+    }
+
+    server.write(client.first_req.join("\r\n") + "\r\n",
+        (err) => {
+            if (err)
+                end();
+            else
+                io.bridge(server, client, end);
+        });
+}
+
+var re_server = /server (\w+):(.+)/;
+var re_host = /host: *(\w+)\.([^:]+)(:[0-9]+)?/i;
+
+var svr = new net.TcpServer(config.port, (c) => {
+    var first_req = ["ok"];
+    var aw = mq.await();
+    var bs = new io.BufferedStream(c);
+    bs.EOL = "\r\n";
+
+    function new_line(err, line) {
+        if (err)
+            return aw.end();
+
+        first_req.push(line);
+
+        var m = re_host.exec(line);
+        if (m) {
+            if (m[2] != config.domain)
+                return c.write(config.error_domain,
+                    () => aw.end());
+
+            bs.first_req = first_req;
+
+            var host = m[1];
+            var server = servers.get(host);
+            bs.aw = aw;
+            if (server)
+                link(server, bs);
+            else
+                clients.put(host, bs);
+        } else
+            bs.readLine(new_line);
+    }
+
+    function first_line(err, line) {
+        if (err)
+            return aw.end();
+
+        var m = re_server.exec(line);
+        if (m) {
+            if (m[2] != "123456") // password
+                return aw.end();
+
+            var host = m[1];
+            var client = clients.get(host);
+            c.aw = aw;
+            if (client)
+                link(c, client);
+            else
+                servers.put(host, c);
+        } else
+            return new_line(err, line);
+    }
+
+    bs.readLine(first_line);
+
+    return aw;
+});
+
+svr.run(() => {});
---
+++ test.js
@@ -0,0 +1,86 @@
+var test = require("test");
+test.setup();
+
+var io = require("io");
+var net = require("net");
+
+run("server.js");
+
+describe("fibyun", () => {
+    describe("server", () => {
+        it("disconnect on error key", () => {
+            var sock = net.connect("tcp://127.0.0.1:9980");
+            sock.write("server lion:1234\r\n");
+            var t1 = new Date();
+            assert.equal(sock.read(), null);
+            assert.lessThan(new Date() - t1, 1000);
+        });
+
+        it("disconnect on timeout", () => {
+            var sock = net.connect("tcp://127.0.0.1:9980");
+            sock.write("server lion:123456\r\n");
+
+            var t1 = new Date();
+            assert.equal(sock.read(), null);
+            assert.greaterThan(new Date() - t1, 5000);
+        });
+    });
+
+    describe("client", () => {
+        it("disconnect on error domain", () => {
+            var sock = net.connect("tcp://127.0.0.1:9980");
+            sock.write("GET / HTTP\r\nhost: lion.baidu.com\r\n");
+            var t1 = new Date();
+            assert.equal(sock.read().toString(), "unknown domain");
+            assert.lessThan(new Date() - t1, 1000);
+        });
+
+        it("disconnect on timeout", () => {
+            var sock = net.connect("tcp://127.0.0.1:9980");
+            sock.write("GET / HTTP\r\nhost: lion.d3j.io\r\n");
+
+            var t1 = new Date();
+            assert.equal(sock.read().toString(), "server not online");
+            assert.greaterThan(new Date() - t1, 5000);
+        });
+
+        it("accept with post", () => {
+            var sock = net.connect("tcp://127.0.0.1:9980");
+            sock.write("GET / HTTP\r\nhost: lion.d3j.io:9980\r\n");
+
+            var t1 = new Date();
+            assert.equal(sock.read().toString(), "server not online");
+            assert.greaterThan(new Date() - t1, 5000);
+        });
+    });
+
+    it("link", () => {
+        var req = [
+            "GET / HTTP",
+            "host: lion.d3j.io",
+            "something else"
+        ];
+
+        var sock = net.connect("tcp://127.0.0.1:9980");
+        sock.write("server lion:123456\r\n");
+
+        var client = net.connect("tcp://127.0.0.1:9980");
+        client.write(req.join("\r\n") + "\r\n");
+
+        var bs = new io.BufferedStream(sock);
+        bs.EOL = "\r\n";
+
+        var t1 = new Date();
+        assert.equal(bs.readLine(), "ok");
+        assert.lessThan(new Date() - t1, 1000);
+
+        req.forEach((line) => {
+            var t1 = new Date();
+            assert.equal(bs.readLine(), line);
+            assert.lessThan(new Date() - t1, 1000);
+        });
+    });
+});
+
+test.run(console.DEBUG);
+process.exit(0);
